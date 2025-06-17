c416a49cacd633038137d4f6b7309095225d8e5c
Nguyễn Văn Hoàng <151611611+hoangnguyen3101@users.noreply.github.com>
2025-06-18 02:05:20 +0700

    Update load_model.py

--- face_module/load_model.py
+++ face_module/load_model.py
@@ -6,10 +6,14 @@ from face_module.models.scrfd import SCRFD
 var = Const()
 
 def load_model():
+    print("📂 Loading model from:", var.det_weight)
+    print("📂 File exists?", os.path.exists(var.det_weight))
     # Sử dụng GPU với ONNX Runtime
-    detector_session = onnxruntime.InferenceSession(var.det_weight, providers=['CUDAExecutionProvider'])
-    recognizer_session = onnxruntime.InferenceSession(var.rec_weight, providers=['CUDAExecutionProvider'])
+    detector_session = onnxruntime.InferenceSession(var.det_weight, providers=['CPUExecutionProvider'])
+    recognizer_session = onnxruntime.InferenceSession(var.rec_weight, providers=['CPUExecutionProvider'])
     
     detector = SCRFD(session=detector_session, model_path=var.det_weight, input_size=(640, 640), conf_thres=var.confidence_thresh)
     recognizer = ArcFace(session=recognizer_session)
+    
+
     return detector, recognizer
