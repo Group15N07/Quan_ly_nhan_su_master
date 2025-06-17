77fb636e68d229d1525c0955436ac1fe3650d19c
Nguyễn Văn Hoàng <151611611+hoangnguyen3101@users.noreply.github.com>
2025-06-18 04:54:59 +0700

    update month.css

--- face_module/c/cConst.py
+++ face_module/c/cConst.py
@@ -1,13 +1,12 @@
+# c/cConst.py
+
 import os
 
 class Const:
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-    det_weight = os.path.join(BASE_DIR, "../face_module/weights/det_10g.onnx")
-    rec_weight = os.path.join(BASE_DIR, "../face_module/weights/w600k_r50.onnx")
-    faces_dir = os.path.join(BASE_DIR, "../app/static/faces")
-    output_images_dir = os.path.join(BASE_DIR, "../face_module/output_images")
-
+    det_weight = r".\face_module\weights\det_10g.onnx"
+    rec_weight = r".\face_module\weights\w600k_r50.onnx"
+    faces_dir = r".\app\static\faces"
+    output_images_dir = r".\face_module\output_images"
     max_num = 1
     similarity_thresh = 0.5
     confidence_thresh = 0.6
